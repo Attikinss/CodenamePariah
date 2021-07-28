16dfe890c8b5a9abc074bfc33b2eb7d4f64d86f3
Dan <daniel@ac.com.au>
2021-07-28 16:51:00 +1000

    Added scripts from my own project into new repo.
    Scripts include the Host Controller, debugging tool stuff, placeholder scripts for weapons/inventory and a Decal script.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+
+    public List<Decal> m_allDecals = new List<Decal>();
+
+    public void TogglePause(bool toggle)
+    { }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDecals();
+    }
+
+    public void AddDecal(Decal decal)
+    {
+        m_allDecals.Add(decal);
+    }
+
+    public void PopDecal()
+    { }
+
+    private void UpdateDecals()
+    {
+        if (m_allDecals.Count > 0)
+        {
+            for (int i = 0; i < m_allDecals.Count; i++)
+            {
+                m_allDecals[i].Update();
+            }
+        }
+    }
+
+	private void OnDrawGizmos()
+	{
+        for (int i = 0; i < m_allDecals.Count; i++)
+        {
+            m_allDecals[i].DrawGizmo();
+        }
+    }
+}
---
+++ Assets/Scripts/Temporary/PlayerManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum WeaponSlot
+{
+    WEAPON1,
+    WEAPON2
+}
+
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager s_Instance;
+
+
+
+    public static WeaponSlot s_CurrentWeapon;
+    public HostController m_PlayerController;
+
+    // Temporary health variable until I get the possession system.
+    public static int s_Health = 100;
+    public static int s_MaxHealth = 100;
+
+
+	private void Awake()
+	{
+        if (PlayerManager.s_Instance != null)
+            Debug.Assert(false); // Make sure there aren't more than one PlayerManager scripts.
+
+        s_Instance = this;           
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        Debug.Assert(m_PlayerController, "Please give the PlayerManager a reference to the PlayerController.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public static void AddHealth(int health)
+    {
+        s_Health += health;
+    }
+
+    public static void RemoveHealth(int health)
+    {
+        s_Health -= health;
+    }
+
+    public static void SetWeapon(WeaponSlot weapon) { s_CurrentWeapon = weapon; }
+
+    public static Weapon GetCurrentWeapon()
+    {
+        PlayerManager playerManager = PlayerManager.s_Instance;
+        Debug.Assert(playerManager.m_PlayerController, "PlayerController is a null reference.");
+
+
+        switch (s_CurrentWeapon)
+        {
+            case WeaponSlot.WEAPON1:
+                return playerManager.m_PlayerController.m_weapon1;
+            case WeaponSlot.WEAPON2:
+                return playerManager.m_PlayerController.m_weapon2;
+            default:
+                return playerManager.m_PlayerController.m_weapon1;
+        }
+    }
+}

7b578aed1223c91bf73092634d0cb1439f965a2e
Dan <daniel@ac.com.au>
2021-08-20 16:18:01 +1000

    Fixed bug where gun would remain firing when swapping to another gun.

--- Assets/Scripts/Player/HostController.cs
+++ Assets/Scripts/Player/HostController.cs
@@ -584,6 +584,8 @@ public class HostController : InputController
     private void SelectWeapon(int index)
     {
         Weapon cache = m_Inventory.m_CurrentWeapon;
+        m_Inventory.m_CurrentWeapon.m_IsAiming = false;
+        m_Inventory.m_CurrentWeapon.m_IsFiring = false;
         m_Inventory.m_CurrentWeapon = m_Inventory.m_Weapons[index];
 
         // Setting them active/inactive to display the correct weapon. Eventually this will be complimented by a weapon swapping phase where it will take some time before

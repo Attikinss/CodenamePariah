c219c9be9d96b8f18b71e181854704db96096e50
Dan <daniel@ac.com.au>
2021-09-03 12:02:52 +1000

    Undid changes to TakeDamage() in Inventory.cs.

--- Assets/Scripts/Combat/Inventory.cs
+++ Assets/Scripts/Combat/Inventory.cs
@@ -66,13 +66,7 @@ public class Inventory : MonoBehaviour
     /// <param name="damage"></param>
     public void TakeDamage(int damage)
     {
-        if (m_Health - damage < 0)
-        { 
-            m_Health = 0;
-        
-        }
-        else
-            m_Health -= damage;
+        m_Health -= damage;
     }
 
     public int GetHealth() { return m_Health; }

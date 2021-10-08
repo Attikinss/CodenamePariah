9c0de91015fcfa8a5fbee94115480eafae2666d0
Cory Atkinson <attikins13@gmail.com>
2021-10-08 13:39:17 +1100

    Updated decorator nodes and added basic AI Director with combat tokens

---
+++ Assets/Scripts/AI/White Willow/Nodes/Decorator/ForceSuccess.cs
@@ -0,0 +1,18 @@
+﻿namespace WhiteWillow.Nodes
+{
+    [Category("Decorator")]
+    public sealed class ForceSuccess : Decorator
+    {
+        protected override void OnEnter() { }
+
+        protected override void OnExit() { }
+
+        protected override NodeResult OnTick()
+        {
+            if (Child == null || Child.Tick() != NodeResult.Running)
+                return NodeResult.Success;
+
+            return NodeResult.Running;
+        }
+    }
+}

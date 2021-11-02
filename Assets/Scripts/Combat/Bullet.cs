25474035aeaf02c10a94f19d75039e5232361237
Dan <daniel@ac.com.au>
2021-11-03 09:12:22 +1100

    Bullets damage player again.

--- Assets/Scripts/Combat/Bullet.cs
+++ Assets/Scripts/Combat/Bullet.cs
@@ -13,10 +13,10 @@ public class Bullet : MonoBehaviour
     private Rigidbody m_Rigidbody;
 
     private void Awake() => m_Rigidbody = GetComponent<Rigidbody>();
-
-    // The owner of the bullet is a reference to the last agent that shot it. It's
-    // used to prevent the bullet from colliding with its own agent.
-    public Transform m_Owner;
+  
+	// The owner of the bullet is a reference to the last agent that shot it. It's
+	// used to prevent the bullet from colliding with its own agent.
+	public Transform m_Owner;
 
     public void SetTarget(GameObject target, int damage)
     {
@@ -33,16 +33,18 @@ public class Bullet : MonoBehaviour
         m_Owner = owner;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.transform != m_Owner) // Prevents collision with it's own agent.
+        if (collision.transform != m_Owner && collision.gameObject.layer != 2) // Prevents collision with it's own agent and things on the ignore raycast layer.
         { 
             if (m_Target != null)
             {
                 if (collision.gameObject == m_Target)
                     m_Target.GetComponent<Inventory>()?.TakeDamage(m_Damage);
 
-                GameManager.s_Instance?.PlaceDecal(collision.transform, collision.GetContact(0).point, collision.GetContact(0).normal);
+
+                // Can't get contact points when using triggers. That's okay because enemies don't need bullet hit markers.
+                //GameManager.s_Instance?.PlaceDecal(collision.transform, collision.GetContact(0).point, collision.GetContact(0).normal);
             }
 
             gameObject.SetActive(false);

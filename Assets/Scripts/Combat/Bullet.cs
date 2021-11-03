940ccf0e148e448e9daf0963cfca5f20ea6c468d
Dan <daniel@ac.com.au>
2021-11-03 18:42:39 +1100

    Bullets no longer SetActive(). Added dirty state to bullets.

--- Assets/Scripts/Combat/Bullet.cs
+++ Assets/Scripts/Combat/Bullet.cs
@@ -12,7 +12,18 @@ public class Bullet : MonoBehaviour
     private int m_Damage = 0;
     private Rigidbody m_Rigidbody;
 
-    private void Awake() => m_Rigidbody = GetComponent<Rigidbody>();
+    private MeshRenderer m_Mesh;
+    private Collider m_Collider;
+
+    [HideInInspector]
+    public bool m_Dirty = true;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
+        m_Mesh = transform.GetChild(0).GetComponent<MeshRenderer>(); // Mesh is on the first child... 
+    }
   
 	// The owner of the bullet is a reference to the last agent that shot it. It's
 	// used to prevent the bullet from colliding with its own agent.
@@ -29,7 +40,11 @@ public class Bullet : MonoBehaviour
         m_Owner = owner;
         transform.position = origin;
         transform.forward = (destination - origin).normalized;
-        gameObject.SetActive(true);
+        //gameObject.SetActive(true);
+
+        // We aren't setting things to active/inactive anymore. Instead we hide the mesh, collider, ect. Also we need to clean the bullets dirty state after we shoot it.
+        m_Dirty = false;
+        Unhide();
         m_Rigidbody.AddForce((destination - origin).normalized * 60.0f, ForceMode.Impulse);
     }
 
@@ -47,10 +62,35 @@ public class Bullet : MonoBehaviour
                 //GameManager.s_Instance?.PlaceDecal(collision.transform, collision.GetContact(0).point, collision.GetContact(0).normal);
             }
 
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+
+            // We aren't setting the object to active/inactive anymore. We also have to set it's dirty state to true after it has been "Destroyed".
+            // This is so the bullet pooler knows that it can use this specific bullet again.
+            m_Rigidbody.velocity = Vector3.zero;
+            m_Dirty = true;
+            Hide();
+            
 
             // Important, if we don't clear the velocity the bullets may stray the next time they are fired!
-            m_Rigidbody.velocity = Vector3.zero;
         }
+    }
+
+    /// <summary>
+    /// Because SetActive() is laggy in the editor, instead of setting the gameobject to false, we will hide the mesh, rigidbody and collider.
+    /// This should virtuall be the same thing but more performant.
+    /// </summary>
+    private void Hide()
+    {
+        m_Collider.enabled = false;
+        m_Rigidbody.isKinematic = true;
+        m_Mesh.enabled = false;
+    }
+
+    private void Unhide()
+    {
+        m_Collider.enabled = true;
+        m_Rigidbody.isKinematic = false;
+        m_Mesh.enabled = true;
+        m_Rigidbody.velocity = Vector3.zero;
     }
 }
